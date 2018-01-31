79656ec771a648c263aa6e9cc8639c816db682e3
SatoruOsawa <osawa@takram.com>
2018-01-31 15:37:35 +0900

    del and gulpSequence added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
+const del = require('del')
 const gulp = require('gulp')
+const gulpSequence = require('gulp-sequence')
+
+// Clean
+
+gulp.task('clean', () => {
+  return del('./build/*')
+})
+
+// Build
 
 gulp.task('html', () => {
   gulp.src('./src/**/*.html')
     .pipe(gulp.dest('./build'))
 })
 
-gulp.task('default', ['html'])
+gulp.task('default', gulpSequence(
+  'clean',
+  'html',
+))
